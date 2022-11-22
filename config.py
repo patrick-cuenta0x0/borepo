fe7a366cc7c30e669626a960722a0446944646ea
Chichokers <developer1575@gmail.com>
2022-11-22 08:15:52 +0100

    aa

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 import ProxyCloud
 
 BOT_TOKEN =  os.environ.get('bot_token','1813335220:AAExLYccktn63NMEgQ2jigmJs8wVKrLvfx4')
-API_ID =  os.environ.get('api_id','18641760')
-API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
+API_ID =  os.environ.get('api_id','28341593')
+API_HASH = os.environ.get('api_hash','13b1607f156d7a7f0c755dbf4ea5061a')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','iamcracks').split(';')
